156a7c3b36488f7ca6881c5497f0178474e4a009
Rustambek <rustambeksafarovv@gmail.com>
2022-08-01 11:46:58 +0500

    Redone

--- bin/basic_class_homework01.dart
+++ bin/basic_class_homework01.dart
@@ -1,5 +1,22 @@
 /*
 Create a "Person" class
 */
+class Person {
+    String name = 'Murod';
+    int age = 17;
 
-void main() {}
+    Person({String name = '', int age = 14}){
+        this.name = name;
+        this.age = age;
+    }
+
+    String getInfo(){
+        return 'My name is $name, I am $age years old';
+    }
+
+}
+
+void main() {
+    Person x = Person(age: 34,name: 'Sardor');
+    print(x.getInfo());
+}
